d5cba784997a8c9f89dd264d9c4b4bbb236ac932
affbiz17-crypto <affbiz17@gmail.com>
2026-05-06 14:37:02 -0700

    Update server.js

--- server.js
+++ server.js
@@ -2,26 +2,12 @@ const express = require("express");
 
 const app = express();
 
-app.use(express.json());
-
 app.get("/", (req, res) => {
-res.send("Server is running");
+  res.send("eBay notification server is running");
 });
 
-app.get("/api/ebay/account-deletion", (req, res) => {
-res.send("eBay account deletion endpoint active");
-});
-
-app.post("/api/ebay/account-deletion", (req, res) => {
-console.log("Deletion notification:", req.body);
-
-res.status(200).json({
-received: true
-});
-});
-
-const PORT = process.env.PORT || 10000;
+const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
-console.log(Server running on port ${PORT});
+  console.log(`Server running on port ${PORT}`);
 });
