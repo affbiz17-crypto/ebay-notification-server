9c90056f77f18ba2e691a8a4e62a91837a80048e
affbiz17-crypto <affbiz17@gmail.com>
2026-05-09 15:59:08 -0700

    Update server.js

--- server.js
+++ server.js
@@ -14,7 +14,24 @@ app.get("/", (req, res) => {
 });
 
 app.get("/connect/ebay", (req, res) => {
-  res.send("eBay login route is working");
+  const scopes = [
+    "https://api.ebay.com/oauth/api_scope",
+    "https://api.ebay.com/oauth/api_scope/sell.fulfillment.readonly",
+    "https://api.ebay.com/oauth/api_scope/sell.inventory.readonly",
+    "https://api.ebay.com/oauth/api_scope/sell.account.readonly",
+    "https://api.ebay.com/oauth/api_scope/sell.marketing.readonly",
+    "https://api.ebay.com/oauth/api_scope/sell.analytics.readonly",
+    "https://api.ebay.com/oauth/api_scope/commerce.identity.readonly"
+  ].join(" ");
+
+  const authUrl =
+    "https://auth.ebay.com/oauth2/authorize" +
+    `?client_id=${encodeURIComponent(process.env.EBAY_CLIENT_ID)}` +
+    `&response_type=code` +
+    `&redirect_uri=${encodeURIComponent(process.env.EBAY_RUNAME)}` +
+    `&scope=${encodeURIComponent(scopes)}`;
+
+  res.redirect(authUrl);
 });
 
 app.get("/auth/ebay/callback", (req, res) => {
